b3d4772ab9eb36098b77a3ae337998a2903396d9
SImone Masiero <masies@usi.ch>
2019-05-17 18:21:30 +0200

    added minsat folder

--- json_io.py
+++ json_io.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, url_for
 
-import random, json, os, sys, getopt, time, pycosat
+import random, json, os, sys, getopt, time, pycosat, itertools
 
 sol = []
+base_scale = [1,2,3,4,5,6,7,8,9]
 
 app = Flask(__name__, static_folder = os.path.abspath(os.path.dirname(sys.argv[0])))
 
@@ -30,71 +31,114 @@ def solve_problem(problemset):
     solve(problemset) 
     return problemset 
     
-def v(i, j, d): 
-    return 81 * (i - 1) + 9 * (j - 1) + d
+def generate_clause(row, col, digit): 
+    return 81 * (row - 1) + 9 * (col - 1) + digit
+
+
 
 #Reduces Sudoku problem to a SAT clauses 
-def sudoku_clauses(): 
-    res = []
-    # for all cells, ensure that the each cell:
-    for i in range(1, 10):
-        for j in range(1, 10):
-            # denotes (at least) one of the 9 digits (1 clause)
-            res.append([v(i, j, d) for d in range(1, 10)])
-            # does not denote two different digits at once (36 clauses)
-            for d in range(1, 10):
-                for dp in range(d + 1, 10):
-                    res.append([-v(i, j, d), -v(i, j, dp)])
+def get_sat_clauses(): 
+    clauses = []
+    # clauses to be satisfied for all the cells:
+    for row in base_scale:
+        for col in base_scale:
+            # each cell denotes at leat one digit (or nothing) (#1)
+            # each clause will have the form
+            # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+            clauses.append([generate_clause(row, col, digit) for digit in base_scale])
+            # each cell denotes at most one digit (none ) (#36)  
+            # each clause will have the form
+            # [-1, -2]
+            # [-1, -3]
+            # [-1, -4]
+            # [-1, -5]
+            # [-1, -6]
+            # [-1, -7]
+            # [-1, -8]
+            # [-1, -9]
+            #  ....
+            for pair in itertools.combinations(base_scale, 2):
+                clauses.append([-generate_clause(row, col, pair[0]), -generate_clause(row, col, pair[1])])
 
-    def valid(cells): 
-        for i, xi in enumerate(cells):
-            for j, xj in enumerate(cells):
-                if i < j:
-                    for d in range(1, 10):
-                        res.append([-v(xi[0], xi[1], d), -v(xj[0], xj[1], d)])
+    # we have 37 * 81 clauses here = 2997
+    # print('lenght = ',len(clauses))
+
 
     # ensure rows and columns have distinct values
-    for i in range(1, 10):
-        valid([(i, j) for j in range(1, 10)])
-        valid([(j, i) for j in range(1, 10)])
+    # basically here it will first create a structure for each cell with : list(itertools.product([row],base_scale))
+    #    (n, 1) (n, 2) ... (n, 9) 
+    # then extract all the pairs from this list where pair_0 < pair_1 with : list(itertools.combinations(list(itertools.product([row],base_scale)), 2))
+    #  ((n, 1), (n, 2)), ((n, 1), (n, 3)),..., ((n, 1), (n, n)), ((n, 2), (n, 3)), ((n, 2), (n, 4)), ...,  ((n, n-1), (n, n))
+    # at this point we have all the pairs of cell on the same row so we can generate the clause to ensure these two cell does not contains the same digit
+    for row in base_scale:
+        for pairs in list(itertools.combinations(list(itertools.product([row],base_scale)), 2)):
+            for digit in base_scale:
+                clauses.append([-generate_clause(pairs[0][0], pairs[0][1], digit), -generate_clause(pairs[1][0], pairs[1][1], digit)])
+
+    # we have 36 (all combination) * 9 (number of columns) * 9 (number of digits) + 2997 (previous clauses) clauses here = 5913
+    # print('lenght = ',len(clauses))
+         
+    # this is actually for cols but can be just incorporated in the previos for loop just inverting the indexes in the pair
+    # but i guess it is enough crazy just like that so have some separation
+    for col in base_scale:
+        for pairs in list(itertools.combinations(list(itertools.product(base_scale,[col])), 2)):
+            for digit in base_scale:
+                clauses.append([-generate_clause(pairs[0][0], pairs[0][1], digit), -generate_clause(pairs[1][0], pairs[1][1], digit)])
+
+    # just like before, another 2916 clauses, raising the total to =  8829
+    # print('lenght = ',len(clauses))
+
         
-    # ensure 3x3 sub-grids "regions" have distinct values
-    for i in 1, 4, 7:
-        for j in 1, 4 ,7:
-            valid([(i + k % 3, j + k // 3) for k in range(9)])
-      
-    assert len(res) == 81 * (1 + 36) + 27 * 324
-    return res
+    # check for 3x3 subregion of the graph (does the same as before intuitively but with grid of 3x3)
+    for cell1 in 1, 4, 7:
+        for cell2 in 1, 4 ,7:
+            for pairs in list(itertools.combinations([(cell1 + k % 3, cell2 + k // 3) for k in range(9)], 2)):
+                for digit in base_scale:
+                    clauses.append([-generate_clause(pairs[0][0], pairs[0][1], digit), -generate_clause(pairs[1][0], pairs[1][1], digit)])
+    # we have 9 square * 9 (number of digits) * 36 (number of pairs) + 8829 (previous clauses) clauses here = 11745
+    # print('lenght = ',len(clauses))
+    
+    # the number final of clauses should be = 11745
+    # 81 cell * 37 condition (part 1)  + (9 rows + 9 cols + 9 grid)  * (9 digits * 36 combination)
+    assert len(clauses) == 81 * (1 + 36) + 27 * 324
+    return clauses
 
 def solve(grid):
-    #solve a Sudoku problem
-    clauses = sudoku_clauses()
-    for i in range(1, 10):
-        for j in range(1, 10):
-            d = grid[i - 1][j - 1]
-            # For each digit already known, a clause (with one literal). 
-            if d:
-                clauses.append([v(i, j, d)])
+    # generate sat clauses
+    clauses = get_sat_clauses()
+    # we must add the digits we already have in the board
+    for row in base_scale:
+        for col in base_scale:
+            if grid[row - 1][col - 1]:
+                clauses.append([generate_clause(row, col, grid[row - 1][col - 1])])
     
-    # Print number SAT clause  
-    numclause = len(clauses)
-    print ("P CNF " + str(numclause) +"(number of clauses)")
-    
-    # solve the SAT problem
-    start = time.time()
+    # run the sat solver
     sol = set(pycosat.solve(clauses))
-    end = time.time()
-    print("Time: "+str(end - start))
     
-    def read_cell(i, j):
-        # return the digit of cell i, j according to the solution
-        for d in range(1, 10):
-            if v(i, j, d) in sol:
-                return d
+    # TODO :: here we can call the minisat solver http://minisat.se/
+    # basically his format is 
+    # p cnf 5 3
+    # 1 -5 4 0
+    # -1 5 3 4 0
+    # -3 -4 0
+    # where in the first row "p cnf 5 3" means that this is SAT problem in CNF format with 5 variables and 3 clauses. and 0 at the end of each line means end of line
 
-    for i in range(1, 10):
-        for j in range(1, 10):
-            grid[i - 1][j - 1] = read_cell(i, j)
+    # our format instead is just
+    # [[1, -5, 4], [-1, 5, 3, 4], [-3, -4]]
+
+    # you can invoke minsat with shell command "minisat [options] [INPUT-FILE [RESULT-OUTPUT-FILE]]"
+
+    # basically we just need to generate a file writing all our clauses plus the first line and we are done
+
+
+    # update the board
+    for row in base_scale:
+        for col in base_scale:
+            for digit in base_scale:
+                if generate_clause(row, col, digit) in sol:
+                    grid[row - 1][col - 1] = digit
+            
+    
 
 
 if __name__ == '__main__':
---
+++ minisat-2.2.0/core/depend.mk
@@ -0,0 +1,9 @@
+/Users/prova/tmp/minisat/core/Main.o /Users/prova/tmp/minisat/core/Main.or /Users/prova/tmp/minisat/core/Main.od /Users/prova/tmp/minisat/core/Main.op: /Users/prova/tmp/minisat/core/Main.cc ../utils/System.h \
+  ../mtl/IntTypes.h ../utils/ParseUtils.h ../utils/Options.h \
+  ../mtl/Vec.h ../mtl/XAlloc.h ../core/Dimacs.h ../core/SolverTypes.h \
+  ../mtl/Alg.h ../mtl/Map.h ../mtl/Alloc.h ../core/Solver.h \
+  ../mtl/Heap.h
+/Users/prova/tmp/minisat/core/Solver.o /Users/prova/tmp/minisat/core/Solver.or /Users/prova/tmp/minisat/core/Solver.od /Users/prova/tmp/minisat/core/Solver.op: /Users/prova/tmp/minisat/core/Solver.cc ../mtl/Sort.h \
+  ../mtl/Vec.h ../mtl/IntTypes.h ../mtl/XAlloc.h ../core/Solver.h \
+  ../mtl/Heap.h ../mtl/Alg.h ../utils/Options.h ../utils/ParseUtils.h \
+  ../core/SolverTypes.h ../mtl/Map.h ../mtl/Alloc.h
